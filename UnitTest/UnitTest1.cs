e15ac8a6b82b41460996332a6942b95b2b640f63
anxnas <slavakhrenov02@gmail.com>
2023-04-10 02:50:46 +0300

    Update

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using pr1;
+using Microsoft.CodeAnalysis;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int i= 1;
+            int j= 2;
+        
+
+            int actual = 0;
+
+            Class1 sol = new Class1(i,j);
+            int s = 0;
+      
+            Assert.AreEqual(actual, sol.Solution(poznu));
+        }
+    }
+}
---
+++ Пр_Хренов1/Пр_Хренов1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr1
+{
+    public class Class1
+    {
+        public Class1(int i, int j)
+        {
+            I = i;
+            J = j;
+
+            int n = i;
+            int m = j;
+
+            int s = 0;
+            int[,] b = new int[n, m];
+
+            for (int ii = 0; ii < n; ii++)
+            {
+                for (int jj = 0; jj < m; jj++)
+                {
+                    b[ii, jj] = Convert.ToInt32(Math.Sin((ii + jj) / 2));
+                    if (b[ii, jj] > 0) s += 1;
+                }
+            }
+
+            Class1_teg(s);
+
+
+            Console.WriteLine("Ответ: число положительных элементов = {0}", s);
+
+        }
+
+        public int Class1_teg(int s)
+        {
+            return s;
+
+        }
+
+
+        public int I { get; set; }
+        public int J { get; set; }
+
+
+
+    }
+}
